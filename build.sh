ca7ca9f367285d8ac3c03824ec07746cb2d0d0e6
Electrux <ckhandelwal13@gmail.com>
2018-04-10 00:11:13 +0530

    Fixed an error of not including string.h in Logger/Core.cpp ( for using strcpy ) and removed color codes from build.sh since they didn't work :P

--- build.sh
+++ build.sh
@@ -20,11 +20,11 @@ mkdir -p "buildfiles/src/Project"
 mkdir -p "bin"
 
 find src -name "*.cpp" | grep -v "tests" | grep -v "main.cpp" | while read -r src; do
-	echo "\033[0;33mCompiling: \033[0;32mbuildfiles/$src.o\033[0m"
+	echo "Compiling: buildfiles/$src.o"
 	$compiler -O2 -std=c++14 -c $src -o buildfiles/$src.o
 done
 
-echo "\033[1;34mBuilding ..."
+echo "Building ..."
 
 buildfiles=`find buildfiles -name "*.cpp.o" | paste -sd " " -`
 $compiler -std=c++14 -lyaml-cpp -lcurl -lpthread -g -o bin/ccp4m src/main.cpp $buildfiles
--- src/Logger/Core.cpp
+++ src/Logger/Core.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <string.h>
 
 #include "../../include/Logger/Core.hpp"
 
