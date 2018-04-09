ce8f8412dd52ecdd51d4bb322185b0e460ab9bc8
Electrux <ckhandelwal13@gmail.com>
2018-04-09 18:33:14 +0530

    Added gcc-6 in dependencies for linux

--- build.sh
+++ build.sh
@@ -17,10 +17,10 @@ echo "Compiling ..."
 
 find src -name "*.cpp" | grep -v "tests" | grep -v "main.cpp" | while read -r src; do
 	echo "buildfiles/$src.o"
-	$compiler -O2 -std=c++11 -c $src -o buildfiles/$src.o
+	$compiler -O2 -std=c++14 -c $src -o buildfiles/$src.o
 done
 
 echo "Building ..."
 
 buildfiles=`find buildfiles -name "*.cpp.o" | paste -sd " " -`
-$compiler -O2 -std=c++11 -lyaml-cpp -lcurl -lpthread -g -o bin/ccp4m src/main.cpp $buildfiles
+$compiler -O2 -std=c++14 -lyaml-cpp -lcurl -lpthread -g -o bin/ccp4m src/main.cpp $buildfiles
