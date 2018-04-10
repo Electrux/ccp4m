5a84181ce263d3e0691fbc4cde2420b8f477a231
Electrux <ckhandelwal13@gmail.com>
2018-04-10 22:04:47 +0530

    Travis linux isn't working so i'll skip it for now. hopefully they release new linux versions sometime soon part 3 typo -_-

--- build.sh
+++ build.sh
@@ -22,7 +22,7 @@ mkdir -p "bin"
 
 find src -name "*.cpp" | grep -v "tests" | grep -v "main.cpp" | while read -r src; do
 	echo "Compiling: $compiler -O2 -std=c++14 -c $src -o buildfiles/$src.o"
-	$compiler -O2 -std=c++11 -c $src -o buildfiles/$src.o
+	$compiler -O2 -std=c++14 -c $src -o buildfiles/$src.o
 	if ! [[ $? == 0 ]]; then
 		break
 	fi
