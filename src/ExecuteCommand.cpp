a673a578f902809489abbb4e8e4f6019ee15a416
Electrux <ckhandelwal13@gmail.com>
2018-04-15 08:40:04 +0530

    Fixed a bug in ExecuteCommand that caused the .ccp4mtmp file to not be deleted ( condition fixed )

--- src/ExecuteCommand.cpp
+++ src/ExecuteCommand.cpp
@@ -22,8 +22,9 @@ int Exec::ExecuteCommand( const std::string & cmd, std::string * err )
 
 	int res = std::system( cmdfinal.c_str() );
 
-	if( err != nullptr && FS::LocExists( Core::TMP_FILE ) ) {
-		* err = FS::ReadFile( Core::TMP_FILE );
+	if( FS::LocExists( Core::TMP_FILE ) ) {
+		if( err != nullptr )
+			* err = FS::ReadFile( Core::TMP_FILE );
 		FS::DeleteFile( Core::TMP_FILE );
 	}
 
