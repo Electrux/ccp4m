aad0625a5c302351c19b7e06667a38b862b69e0b
Electrux <ckhandelwal13@gmail.com>
2018-04-21 11:15:45 +0530

    Updated license core url, version now 0.5.3

--- src/Core.cpp
+++ src/Core.cpp
@@ -18,7 +18,7 @@
 
 const int Core::VERSION_PRIMARY = 0;
 const int Core::VERSION_SECONDARY = 5;
-const int Core::VERSION_MINOR = 2;
+const int Core::VERSION_MINOR = 3;
 
 Core::Arch Core::arch =
 #ifdef __linux__
